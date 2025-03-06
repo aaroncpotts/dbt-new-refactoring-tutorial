91732c038ab3177ee0c60ddaa71045c0ef43f94a
Aaron Potts <aaron.potts@theinformationlab.com>
2025-03-06 14:20:56 +0000

    pulled out CTEs from mainline SQL, reordered CTEs to put paid_orders prior to some logic that required it

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -30,49 +30,55 @@ from payments
 where status <> 'fail'
 group by 1),
 
--- Final CTE
--- Simple Select Statment
-paid_orders as (select orders.id as order_id,
+paid_orders as 
+    (select orders.id as order_id,
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
-            orders.status as order_status,
+        orders.status as order_status,
         aggregate_payments.total_amount_paid,
         aggregate_payments.payment_finalized_date,
-        c.first_name    as customer_first_name,
-            c.last_name as customer_last_name
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
     left join aggregate_payments on orders.id = aggregate_payments.order_id
-left join customers c on orders.user_id = c.id ),
+    left join customers on orders.user_id = customers.id 
+),
 
 customer_orders 
-    as (select c.id as customer_id
+    as (select customers.id as customer_id
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(orders.id) as number_of_orders
-    from customers c 
+    from customers 
     left join orders  as orders
-    on orders.user_id = c.id 
-    group by 1)
+    on orders.user_id = customers.id 
+    group by 1),
+
+lifetime_value_generate as (
+            select
+            paid_orders.order_id,
+            sum(t2.total_amount_paid) as clv_bad
+        from paid_orders
+        left join paid_orders t2 on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
+        group by 1
+        order by paid_orders.order_id
+
+)
+
+-- Final CTE
+-- Simple Select Statment
+
 
 select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case when c.first_order_date = p.order_placed_at
+    paid_orders.*,
+    row_number() over (order by paid_orders.order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+    case when customer_orders.first_order_date = paid_orders.order_placed_at
     then 'new'
     else 'return' end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-    from paid_orders p
-    left join customer_orders as c using (customer_id)
-    left outer join 
-    (
-            select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x on x.order_id = p.order_id
+    lifetime_value_generate.clv_bad as customer_lifetime_value,
+    customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
+    left outer join lifetime_value_generate on lifetime_value_generate.order_id = paid_orders.order_id
     order by order_id
