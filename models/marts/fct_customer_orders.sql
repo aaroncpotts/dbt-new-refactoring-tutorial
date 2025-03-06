275ccb733ae23cbdae4efbc7cafc9a2efdd4d55c
Aaron Potts <aaron.potts@theinformationlab.com>
2025-03-06 15:05:38 +0000

    Rewrite first_day_of_sale and new customer as window functions, including my first exposure to first_value

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -22,7 +22,7 @@ aggregate_payments as (
 paid_orders as 
     (select orders.id as order_id,
         orders.user_id as customer_id,
-        --order_placed_at used to order the customer_lifetime_value later
+        --order_placed_at used to order the customer_lifetime_value and other window functions later
         orders.order_date as order_placed_at,
         orders.status as order_status,
         aggregate_payments.total_amount_paid,
@@ -34,15 +34,6 @@ paid_orders as
     left join customers on orders.user_id = customers.id 
 ),
 
-customer_orders 
-    as (select customers.id as customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-    from customers 
-    left join orders
-    on orders.user_id = customers.id 
-    group by 1),
 
 
 -- Final CTE
@@ -53,8 +44,15 @@ final as (select
     row_number() over (order by paid_orders.order_id) as transaction_seq,
     --customer sales sequence - partitioned by customer, ordered by order ID
     row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
-    case when customer_orders.first_order_date = paid_orders.order_placed_at
-    then 'new'
+    /* first order date - use window function partitioned by order_id to arrange orders by order_placed_at, then return 'new' if
+    it's the first ranked order*/
+    case
+         when (
+            rank() over (
+                partition by customer_id
+                order by order_placed_at, order_id
+            ) = 1
+        ) then 'new'
     else 'return' end as nvsr,
     /*customer lifetime value - sum of total_amount_paid from paid orders, partitioned by customer id
     then ordered by the order_placed_at value from paid_orders*/
@@ -62,9 +60,13 @@ final as (select
         partition by paid_orders.customer_id
         order by paid_orders.order_placed_at
         ) as customer_lifetime_value,
-    customer_orders.first_order_date as fdos
+    /* hajimete no first_value - SQL Server 2012 window function. Thing you're finding the first order for, over() what you're partitioning by
+    (in this case, customer_id), what you're ordering by */
+    first_value(paid_orders.order_placed_at) over(
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at
+    ) as fdos
     from paid_orders
-    left join customer_orders using (customer_id)
     order by order_id)
 -- Simple Select Statment
 
